95d56d6c5e7cce0f0a4d3162aea22d8968b4a28b
Iwan Zotow <iwanzotow@gmail.com>
2014-07-29 23:27:15 -0400

    Added CT Slice collection and tags files

---
+++ DICOMViewer/DICOMViewer.Helper/CTSliceInfoCollection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DICOMViewer.Helper
+{
+    class CTSliceInfoCollection
+    {
+        private Dictionary<string, CTSliceInfo> _slices_by_name = null;
+        private Dictionary<int, CTSliceInfo>    _slices_by_locn = null;
+
+        public CTSliceInfoCollection()
+        {
+        }
+
+        public bool Add(CTSliceInfo ct)
+        {
+            string fname = ct.FileName;
+            int sliceloc = ct.SliceLoc;
+
+            _slices_by_name.Add(fname, ct);
+            _slices_by_locn.Add(sliceloc, ct);
+
+            return true;
+        }
+    }
+}
---
+++ DICOMViewer/DICOMViewer.Parsing/DICOMtags.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DICOMViewer.Parsing
+{
+    class DICOMTAG
+    {
+        public const string WINDOW_CENTER = "(0028,1050)";
+        public const string WINDOW_WIDTH  = "(0028,1051)";
+        public const string ROWS    = "(0028,0010)";
+        public const string COLUMNS = "(0028,0011)";
+
+        public const string IMAGE_PLANE_PIXEL_SPACING = "(3002,0011)";
+
+        public const string IMAGE_POSITION_PATIENT = "(0020,0032)";
+        public const string SLICE_LOCATION         = "(0020,1041)";
+    }
+}
