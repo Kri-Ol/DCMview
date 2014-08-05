a8b9177dafcf20c983533603f77a9daae83d883c
Iwan Zotow <iwanzotow@gmail.com>
2014-08-05 10:59:25 -0400

    Added Utilities

---
+++ Utilities/Utils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+    public sealed class Utils
+    {
+        public static float squared(float x)
+        {
+            return x * x;
+        }
+
+        public static double squared(double x)
+        {
+            return x * x;
+        }
+
+        public static float cubed(float x)
+        {
+            return x * x * x;
+        }
+
+        public static double cubed(double x)
+        {
+            return x * x * x;
+        }
+    }
+}
